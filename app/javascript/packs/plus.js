c23c7fb99420425443c0bf4b6f2291a2fb2109f4
David Touitou <david.touitou.dt@gmail.com>
2019-08-27 19:56:46 +0200

    new_survey_from_new_event

---
+++ app/javascript/packs/plus.js
@@ -0,0 +1,13 @@
+// // clic sur le "+" de survey (dates)
+// const addDatePossibility = () => {
+//   const
+
+//   participantsNb.addEventListener("change", (event) => {
+//     const part = participantsNb.value;
+//     calcul.innerHTML = part * priceWoCurrency;
+//   });
+// }
+
+// if (document.querySelector("#booking_number_of_participants")) {
+//   initPrice();
+// }
