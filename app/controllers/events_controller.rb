416ce3b98c33be7f6f7bbae9e653519a435973f2
Camille Dethorey <camille.dethorey@gmail.com>
2019-08-27 15:06:20 +0200

    add tabs nav in view new event

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,20 @@
+class EventsController < ApplicationController
+
+  def new
+    @event = Event.new
+    authorize @event
+  end
+
+  def create
+    @event = Event.new(params_event)
+    @event.save
+    redirect_to event_path(@event)
+    authorize @event
+  end
+
+  private
+
+  def params_event
+    params.require(:event).permit(:name, :event_category, :thematics, :start_date, :end_date, :destination, :budget_per_participant, :photo, :deadline, :token)
+  end
+end
