04f7f3bd8bf39b3d329d38a346815af963fedd68
Camille Dethorey <camille.dethorey@gmail.com>
2019-08-27 13:05:47 +0200

    add default value to boolean present

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_104419) do
+ActiveRecord::Schema.define(version: 2019_08_27_110039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2019_08_27_104419) do
   end
 
   create_table "user_events", force: :cascade do |t|
-    t.boolean "present"
+    t.boolean "present", default: false
     t.bigint "user_id"
     t.bigint "event_id"
     t.datetime "created_at", null: false
