db515f7177a0d2433fc4b41fe232485da1960b29
Camille Dethorey <camille.dethorey@gmail.com>
2019-08-29 19:20:58 +0200

    fix multiple choices

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -2,15 +2,17 @@ class ChoicesController < ApplicationController
 
   def create
 
-    @choice = Choice.new(params_choice)
-    @choice.user = current_user
     results = params[:choice][:suggestion_id]
     if results.length == 1
+      @choice = Choice.new(params_choice)
+      @choice.user = current_user
       @choice.suggestion = Suggestion.find(results)
       @choice.save!
     else
       cleaned_results = results.drop(1)
       cleaned_results.each do |result|
+        @choice = Choice.new(params_choice)
+        @choice.user = current_user
         @choice.suggestion = Suggestion.find(result)
         @choice.save!
       end
