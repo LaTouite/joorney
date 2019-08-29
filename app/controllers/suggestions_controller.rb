98c1cd2c235dcbf19632cb91abf0e25bebd557e1
Camille Dethorey <camille.dethorey@gmail.com>
2019-08-29 17:12:37 +0200

    some tests on form choices

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -4,6 +4,8 @@ class SuggestionsController < ApplicationController
     @event = Event.find(params[:event_id])
     @survey = Survey.find_by(event_id: @event.id)
     @suggestions = policy_scope(Suggestion).where(survey_id: @survey.id)
+    @suggestions_dates = @suggestions.where(topic: "Dates")
+    @suggestions_destination = @suggestions.where(topic: "Destination")
     @choice = Choice.new
   end
 end
