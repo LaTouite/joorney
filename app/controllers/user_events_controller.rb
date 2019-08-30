34d89785189ee38867374b0fc2766b63eccfad93
theo rouland <theo.rouland@laposte.net>
2019-08-30 10:08:37 +0200

    good

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -37,6 +37,7 @@ class UserEventsController < ApplicationController
       to: "whatsapp:+33#{user_event.phone_number[1..-1]}",
       body: "Salut. localhost:3000/user_events/#{user_event.event.id}/confirm_invitation?token=#{user_event.event.token}"
     )
+    redirect_to event_path(user_event.event_id)
   end
 
   def user_event_params
