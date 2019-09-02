d38d7e337183561391412e7ca536df0273405c7e
Camille Dethorey <camille.dethorey@gmail.com>
2019-09-02 20:58:13 +0200

    modify navbar and tabs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -58,7 +58,6 @@ if (document.querySelector("#event_activity_nb_of_participants")) {
 const initToggleDate = () => {
   const toggle_input_date = document.querySelector("#checkbox-toggle-survey-date");
   toggle_input_date.addEventListener("click", (event) => {
-    event.preventDefault();
     const start_date = document.querySelector("#event_start_date_full");
     console.log(start_date);
     start_date.classList.toggle("d-none");
@@ -76,7 +75,6 @@ if (document.querySelector("#checkbox-toggle-survey-date")) {
 const initTogglePlace = () => {
   const toggle_input_place = document.querySelector("#checkbox-toggle-survey-place");
   toggle_input_place.addEventListener("click", (event) => {
-    event.preventDefault();
     const destination = document.querySelector("#event_destination");
     destination.classList.toggle("d-none");
     destination.removeAttribute('disabled');
@@ -90,7 +88,6 @@ if (document.querySelector("#checkbox-toggle-survey-place")) {
 const initToggleBudget = () => {
   const toggle_input_budget = document.querySelector("#checkbox-toggle-survey-budget");
   toggle_input_budget.addEventListener("click", (event) => {
-    event.preventDefault();
     const budget = document.querySelector("#event_budget_per_participant_cents");
     budget.classList.toggle("d-none");
     budget.removeAttribute('disabled');
