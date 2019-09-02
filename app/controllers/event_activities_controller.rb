62fff2e53b1193473388282812beed058663fcb7
Camille Dethorey <camille.dethorey@gmail.com>
2019-09-02 11:17:02 +0200

    work on controller event_activities

--- app/controllers/event_activities_controller.rb
+++ app/controllers/event_activities_controller.rb
@@ -3,7 +3,9 @@ class EventActivitiesController < ApplicationController
 
   def create
     @event_activity = EventActivity.new(favorite: true)
-
+    @event_activity.event = Event.where(id: params[:event_id])
+    @event_activity.activity = Activity.where(id: params[:activity_id])
+    @event_activity.save
 
     authorize @event_activity
   end
